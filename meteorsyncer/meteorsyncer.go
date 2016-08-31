b7ab7377b8bbab4259e98fd87cb6af69510c7236
Anthony Bobsin <bobsinj@gmail.com>
2016-08-30 22:40:34 -0400

    Runs go fmt on meteorsyncer.go and server.go to fix indentation

--- meteorsyncer/meteorsyncer.go
+++ meteorsyncer/meteorsyncer.go
@@ -1,71 +1,71 @@
 package main
 
 import (
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
-  "fmt"
-  "encoding/json"
-  "net/http"
-  "os"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"os"
 )
 
 type Meteor struct {
-  Name string     `json:"name"`
-  Id string       `json:"id"`
-  NameType string `json:"nametype"`
-  Class string    `json:"recclass"`
-  Fall string     `json:"fall"`
-  MassG string    `json:"mass"`
-  Date string     `json:"year"`
-  Lat string      `json:"reclat"`
-  Long string     `json:"reclong"`
+	Name     string `json:"name"`
+	Id       string `json:"id"`
+	NameType string `json:"nametype"`
+	Class    string `json:"recclass"`
+	Fall     string `json:"fall"`
+	MassG    string `json:"mass"`
+	Date     string `json:"year"`
+	Lat      string `json:"reclat"`
+	Long     string `json:"reclong"`
 }
 
 func (m Meteor) formattedMassG() string {
-  if len(m.MassG) == 0 {
-    return "0"
-  } else {
-    return m.MassG
-  }
+	if len(m.MassG) == 0 {
+		return "0"
+	} else {
+		return m.MassG
+	}
 }
 
 func getDBConnection() (*sql.DB, error) {
-  db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/nasa_datasets", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASS")))
-  if err != nil {
-    panic(err.Error())
-  }
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/nasa_datasets", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASS")))
+	if err != nil {
+		panic(err.Error())
+	}
 
-  return db, err
+	return db, err
 }
 
 func getJson(url string, target interface{}) error {
-  r, err := http.Get(url)
-  if err != nil {
-    return err
-  }
-  defer r.Body.Close()
+	r, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
 
-  return json.NewDecoder(r.Body).Decode(target)
+	return json.NewDecoder(r.Body).Decode(target)
 }
 
 func main() {
-  db, err := getDBConnection()
-  defer db.Close()
+	db, err := getDBConnection()
+	defer db.Close()
 
-  // Prepare statement for inserting data TODO: use multi insert
-  stmtIns, err := db.Prepare("INSERT IGNORE INTO meteor_landings VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ? )")
-  if err != nil {
-    panic(err.Error())
-  }
-  defer stmtIns.Close()
+	// Prepare statement for inserting data TODO: use multi insert
+	stmtIns, err := db.Prepare("INSERT IGNORE INTO meteor_landings VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ? )")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmtIns.Close()
 
-  var meteors []Meteor
-  getJson("https://data.nasa.gov/resource/y77d-th95.json", &meteors)
+	var meteors []Meteor
+	getJson("https://data.nasa.gov/resource/y77d-th95.json", &meteors)
 
-  for _,meteor := range meteors {
-    _, err := stmtIns.Exec(meteor.Id, meteor.Name, meteor.NameType, meteor.Class, meteor.Fall, meteor.formattedMassG(), meteor.Date, meteor.Lat, meteor.Long)
-    if err != nil {
-      panic(err.Error())
-    }
-  }
+	for _, meteor := range meteors {
+		_, err := stmtIns.Exec(meteor.Id, meteor.Name, meteor.NameType, meteor.Class, meteor.Fall, meteor.formattedMassG(), meteor.Date, meteor.Lat, meteor.Long)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,74 +1,74 @@
 package main
 
 import (
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
-  "encoding/json"
-  "net/http"
-  "fmt"
-  "os"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"os"
 )
 
 type Meteor struct {
-  NasaId   int
-  Name     string
-  NameType string
-  Class    string
-  Fall     string
-  MassG    int32
-  Date     string
-  Lat      float32
-  Long     float32
+	NasaId   int
+	Name     string
+	NameType string
+	Class    string
+	Fall     string
+	MassG    int32
+	Date     string
+	Lat      float32
+	Long     float32
 }
 
 func getDBConnection() (*sql.DB, error) {
-  db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/nasa_datasets", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASS")))
-  if err != nil {
-    panic(err.Error())
-  }
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/nasa_datasets", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASS")))
+	if err != nil {
+		panic(err.Error())
+	}
 
-  return db, err
+	return db, err
 }
 
 func getMeteors(w http.ResponseWriter, r *http.Request) {
-  db, err := getDBConnection()
-  defer db.Close()
+	db, err := getDBConnection()
+	defer db.Close()
 
-  rows, err := db.Query("SELECT * FROM meteor_landings")
-  if err != nil {
-    panic(err.Error())
-  }
-  defer rows.Close()
+	rows, err := db.Query("SELECT * FROM meteor_landings")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
 
-  var (
-    meteor Meteor
-    meteors []Meteor
-  )
+	var (
+		meteor  Meteor
+		meteors []Meteor
+	)
 
-  for rows.Next() {
-  	err := rows.Scan(&meteor.NasaId,
-                    &meteor.Name,
-                    &meteor.NameType,
-                    &meteor.Class,
-                    &meteor.Fall,
-                    &meteor.MassG,
-                    &meteor.Date,
-                    &meteor.Lat,
-                    &meteor.Long)
-  	if err != nil {
-      panic(err.Error())
-  	}
-    meteors = append(meteors, meteor)
-  }
-  err = rows.Err()
-  if err != nil {
-    panic(err)
-  }
+	for rows.Next() {
+		err := rows.Scan(&meteor.NasaId,
+			&meteor.Name,
+			&meteor.NameType,
+			&meteor.Class,
+			&meteor.Fall,
+			&meteor.MassG,
+			&meteor.Date,
+			&meteor.Lat,
+			&meteor.Long)
+		if err != nil {
+			panic(err.Error())
+		}
+		meteors = append(meteors, meteor)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
 
-  json.NewEncoder(w).Encode(meteors)
+	json.NewEncoder(w).Encode(meteors)
 }
 
 func main() {
-  http.HandleFunc("/meteors", getMeteors)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/meteors", getMeteors)
+	http.ListenAndServe(":8080", nil)
 }
