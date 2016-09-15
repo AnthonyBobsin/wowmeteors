fd28ff2f730aa133ad254bf5b24a27e6ee6eb275
Anthony Bobsin <bobsinj@gmail.com>
2016-09-15 00:03:19 -0400

    Changes MeteorDashboard stateless functional component to es6 class

--- src/routes/MeteorDashboard/components/MeteorDashboard.js
+++ src/routes/MeteorDashboard/components/MeteorDashboard.js
@@ -2,40 +2,53 @@ import React from 'react'
 
 import './MeteorDashboard.scss'
 
-export const MeteorDashboard = (props) => {
-  const handleChangeValue = (event) => props.changeValue(event.target.value)
-  const handleAddMeteor = (event) => props.addMeteor(props.value)
-  let addMeteorClasses = "btn btn-primary add-meteor"
+export class MeteorDashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChangeValue = this.handleChangeValue.bind(this)
+    this.handleAddMeteor = this.handleAddMeteor.bind(this)
+  }
 
-  if (!props.value) addMeteorClasses += " disabled"
+  handleChangeValue (event) {
+    this.props.changeValue(event.target.value)
+  }
 
-  return (
-    <div className="meteor-dashboard">
-      <h5>Welcome To</h5>
-      <h3>The Meteor Dashboard</h3>
-      <br/>
-      <div className="form-inline">
-        <input
-          type="text"
-          className="meteor-value form-control"
-          placeholder="Meteor Name"
-          value={props.value}
-          onChange={handleChangeValue}
-        />
-        <button
-          className={addMeteorClasses}
-          onClick={handleAddMeteor}
-        >
-          Add Meteor
-        </button>
+  handleAddMeteor (event) {
+    if (this.props.value) this.props.addMeteor(this.props.value)
+  }
+
+  render () {
+    let addMeteorClasses = "btn btn-primary add-meteor"
+    if (!this.props.value) addMeteorClasses += " disabled"
+
+    return (
+      <div className="meteor-dashboard">
+        <h5>Welcome To</h5>
+        <h3>The Meteor Dashboard</h3>
+        <br/>
+        <div className="form-inline">
+          <input
+            type="text"
+            className="meteor-value form-control"
+            placeholder="Meteor Name"
+            value={this.props.value}
+            onChange={this.handleChangeValue}
+          />
+          <button
+            className={addMeteorClasses}
+            onClick={this.handleAddMeteor}
+          >
+            Add Meteor
+          </button>
+        </div>
+        <div>
+          {this.props.meteors.map((meteor, i) => (
+            <div key={i}>{meteor}</div>
+          ))}
+        </div>
       </div>
-      <div>
-        {props.meteors.map((meteor, i) => (
-          <div key={i}>{meteor}</div>
-        ))}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 MeteorDashboard.propTypes = {
