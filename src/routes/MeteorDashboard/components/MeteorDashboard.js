f64bd967c247d601c3939b6ace1778ad30dd16de
Anthony Bobsin <bobsinj@gmail.com>
2016-09-14 23:24:35 -0400

    Cosmetic tweaks to add meteor forms

--- src/routes/MeteorDashboard/components/MeteorDashboard.js
+++ src/routes/MeteorDashboard/components/MeteorDashboard.js
@@ -1,15 +1,35 @@
 import React from 'react'
 
+import './MeteorDashboard.scss'
+
 export const MeteorDashboard = (props) => {
   const handleChangeValue = (event) => props.changeValue(event.target.value)
   const handleAddMeteor = (event) => props.addMeteor(props.value)
+  let addMeteorClasses = "btn btn-primary add-meteor"
+
+  if (!props.value) addMeteorClasses += " disabled"
 
   return (
-    <div>
-      <input type="text" value={props.value} onChange={handleChangeValue}/>
-      <button onClick={handleAddMeteor}>
-        Click me!
-      </button>
+    <div className="meteor-dashboard">
+      <h5>Welcome To</h5>
+      <h3>The Meteor Dashboard</h3>
+      <br/>
+      <form className="form-inline">
+        <input
+          type="text"
+          className="meteor-value form-control"
+          placeholder="Meteor Name"
+          value={props.value}
+          onChange={handleChangeValue}
+        />
+        <button
+          type="button"
+          className={addMeteorClasses}
+          onClick={handleAddMeteor}
+        >
+          Add Meteor
+        </button>
+      </form>
       <div>
         {props.meteors.map((meteor, i) => (
           <div key={i}>{meteor}</div>
