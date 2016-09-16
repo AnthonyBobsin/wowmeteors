1c59c7024c1190082b07e451194f711b88fd8071
Anthony Bobsin <bobsinj@gmail.com>
2016-09-16 01:53:02 -0400

    Simplifies server.go header setting (Access-Control-Allow-Origin) until a proper adapter is implemented

--- server.go
+++ server.go
@@ -10,12 +10,14 @@ import (
 )
 
 func getMeteors(w http.ResponseWriter, r *http.Request) {
-	if origin := r.Header.Get("Origin"); origin != "" {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// if origin := r.Header.Get("Origin"); origin != "" {
+	// 	w.Header().Set("Access-Control-Allow-Origin", origin)
+	// 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	// 	w.Header().Set("Access-Control-Allow-Headers",
+	// 		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// }
 
 	json.NewEncoder(w).Encode(
 		models.AllMeteors(
